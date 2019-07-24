ea0171e7eab4c1591816338c1e331967e57bae28
Paola <paomartcard@gmail.com>
2019-07-24 00:12:27 -0700

    major changes
    jajaja demasiado

--- js/custom.js
+++ js/custom.js
@@ -104,9 +104,12 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
+    $("#demo01,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
 
-    // Contact Form 	
+    //jardinAllende
+    $("#demo02").jardinAllende();
+
+    // Contact Form
 
     // validate contact form
     $(function () {
