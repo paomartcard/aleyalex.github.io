356f3e9a615f8b5817043b7e7ef9bd2aa2f3b250
Paola <paomartcard@gmail.com>
2019-08-13 22:40:18 -0700

    al fin!

---
+++ js/password.js
@@ -0,0 +1,37 @@
+function setCookie(cname,cvalue) {
+  document.cookie = cname + "=" + cvalue + ";";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var pass=getCookie("password");
+  if (pass != "aleyalex") {
+    pass = prompt("Please enter the password:","");
+    if (pass == "aleyalex") {
+      setCookie("password", "aleyalex");
+    }else{
+      while (true) {
+        pass = prompt("Incorrect. Please enter the password:","");
+        if (pass == "aleyalex") {
+          setCookie("password", "aleyalex");
+          break;
+        }
+      }
+    }
+  }
+}
